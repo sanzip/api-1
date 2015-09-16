862a87f49c08e25941773facc9add4e9ec2b1c75
Prasanna Pandey <ppandey@deerwalk.com>
2015-09-16 17:06:28 +0545

    Changed dev datasource from 'create-drop' to 'update'

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -18,7 +18,7 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
             url = "jdbc:mysql://localhost:3306/dwit_self_test"
         }
     }
